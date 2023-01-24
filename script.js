346a05b663890249ad3f39e76c42c06a2324cdee
Hernando Abella <hernandoabella@gmail.com>
2023-01-24 18:08:16 -0500

    refactorizacion de codigo

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-let agente = navigator.userAgent.toLocaleLowerCase();  
-let navegador;  
+function detectarNavegador() {
+    let agente = navigator.userAgent.toLowerCase();
+    let navegador = "otro"; // asumimos que es otro navegador
 
-if (agente.indexOf("edge") > -1) {
-    console.log("MS Edge");
-} else if (agente.indexOf("edg/") > -1) {
-    console.log("Edge ( chromium based)");
-    navegador = "edge";
-} else if (agente.indexOf("opr") > -1 && !!window.opr) {
-    console.log("Opera");
-    navegador = "opera";
-} else if (agente.indexOf("chrome") > -1 && !!window.chrome) {
-    console.log("Chrome");
-    navegador = "chrome";
-} else if (agente.indexOf("trident") > -1) {
-    console.log("MS IE");
-} else if (agente.indexOf("firefox") > -1){
-    console.log("Mozilla Firefox");
-    navegador = "firefox";
-} else if (agente.indexOf("safari") > -1) {
-    console.log("Safari");
-    navegador = "safari";
-} else {
-    alert("Estás utilizando otro navegador");
+    if (agente.indexOf("edge") > -1) {
+        navegador = "edge";
+    } else if (agente.indexOf("edg/") > -1) {
+        navegador = "edge";
+    } else if (agente.indexOf("opr") > -1 && !!window.opr) {
+        navegador = "opera";
+    } else if (agente.indexOf("chrome") > -1 && !!window.chrome) {
+        navegador = "chrome";
+    } else if (agente.indexOf("trident") > -1) {
+        navegador = "ie";
+    } else if (agente.indexOf("firefox") > -1){
+        navegador = "firefox";
+    } else if (agente.indexOf("safari") > -1) {
+        navegador = "safari";
+    }
+
+    document.querySelector('.navegador').innerHTML = navegador[0].toUpperCase() + navegador.slice(1);
+
+    const logo = document.querySelector(`.logos .${navegador}`);
+
+    if(logo !== ""){  
+        logo.style.opacity = "1";
+    }
 }
 
-// Convierte la primera letra de la salida en mayúsculas
-document.querySelector('.navegador').innerHTML = navegador[0].toUpperCase() + navegador.slice(1);
-
-const logo = document.querySelector(`.logos .${navegador}`);
-
-if(logo !== ""){  
-    logo.style.opacity = "1";
-}
+detectarNavegador();
