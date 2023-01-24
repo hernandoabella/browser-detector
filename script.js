bf842660f04fd1f79f9541d50ef9cc761c043846
Hernando Abella <hernandoabella@gmail.com>
2023-01-24 02:15:35 -0500

    mejoras completadas

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-let userAgent = navigator.userAgent;  
-let browser;  
-if(userAgent.match(/edg/i)){  
-    browser = "edge";  
-}else if(userAgent.match(/firefox|fxios/i)){  
-    browser = "firefox";  
-}else if(userAgent.match(/opr//i)){  
-    browser = "opera";  
-}else if(userAgent.match(/chrome|chromium|crios/i)){  
-    browser = "chrome";  
-}else if(userAgent.match(/safari/i)){  
-    browser = "safari";  
-}else{  
-    alert("Otro navegador");  
+let agente = navigator.userAgent.toLocaleLowerCase();  
+let navegador;  
+
+if (agente.indexOf("edge") > -1) {
+    console.log("MS Edge");
+} else if (agente.indexOf("edg/") > -1) {
+    console.log("Edge ( chromium based)");
+    navegador = "edge";
+} else if (agente.indexOf("opr") > -1 && !!window.opr) {
+    console.log("Opera");
+    navegador = "opera";
+} else if (agente.indexOf("chrome") > -1 && !!window.chrome) {
+    console.log("Chrome");
+    navegador = "chrome";
+} else if (agente.indexOf("trident") > -1) {
+    console.log("MS IE");
+} else if (agente.indexOf("firefox") > -1){
+    console.log("Mozilla Firefox");
+    navegador = "firefox";
+} else if (agente.indexOf("safari") > -1) {
+    console.log("Safari");
+    navegador = "safari";
+} else {
+    alert("Estás utilizando otro navegador");
 }
 
-const logo = document.querySelector(`.logos .${browser}`);  
+// Convierte la primera letra de la salida en mayúsculas
+document.querySelector('.navegador').innerHTML = navegador[0].toUpperCase() + navegador.slice(1);
 
-if(logo != ""){  
+const logo = document.querySelector(`.logos .${navegador}`);
+
+if(logo !== ""){  
     logo.style.opacity = "1";
-    logo.style.transform = "scale(1)";  
-}  
+}
